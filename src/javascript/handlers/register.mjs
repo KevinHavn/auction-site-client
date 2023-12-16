6027fc227a9e49dfdbf11b3623fb008711660753
KevinHavn <kevin-havn@hotmail.com>
2023-12-17 00:20:09 +0100

    added login and logout handlers

---
+++ src/javascript/handlers/register.mjs
@@ -0,0 +1,13 @@
+import { register } from "../auth/register.mjs";
+const form = document.querySelector("#registerform");
+
+export function setRegisterFormListener() {
+	form.addEventListener("submit", (event) => {
+		event.preventDefault();
+		const form = event.target;
+		const formData = new FormData(form);
+		const profile = Object.fromEntries(formData.entries());
+
+		register(profile);
+	});
+}
--- src/javascript/index.mjs
+++ src/javascript/index.mjs
@@ -1,5 +1,10 @@
 import { loadPosts } from "./listings/test.mjs";
 import { createPostsTemplate } from "./listings/templates/posttemplate.mjs";
+import { setRegisterFormListener } from "./handlers/register.mjs";
+import { setLoginFormListener } from "./handlers/login.mjs";
+
 
 loadPosts();
 createPostsTemplate()
+setRegisterFormListener();
+setLoginFormListener();
