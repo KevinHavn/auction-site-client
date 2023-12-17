c8b73d1e1c383519a1cb42c966cca48f3dd6ef5a
Kevin Havn <kevin-havn@hotmail.com>
2023-12-17 22:16:04 +0100

    added event handler for listing form

--- src/javascript/index.mjs
+++ src/javascript/index.mjs
@@ -1,13 +1,13 @@
-import { loadPosts } from "./listings/test.mjs";
 import { createPostsTemplate } from "./listings/templates/posttemplate.mjs";
 import { setRegisterFormListener } from "./handlers/register.mjs";
 import { setLoginFormListener } from "./handlers/login.mjs";
 import { setLogoutListener } from "./handlers/logout.mjs";
 import { setUpdateFormListener } from "./handlers/update.mjs";
+import { setListingFormListener } from "./handlers/post.mjs";
 
-loadPosts();
 createPostsTemplate();
 setRegisterFormListener();
 setLoginFormListener();
 setLogoutListener();
 setUpdateFormListener(); 
+setListingFormListener();
